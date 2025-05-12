a88ac987665a3eceec698be9738e66d2c12cac54
AntoineDecauchy <decauchy.antoine@gmail.com>
2025-05-12 04:34:15 +0200

    on avance

--- drawing/drawing.c
+++ drawing/drawing.c
@@ -12,24 +12,6 @@
 
 #include "../so_long.h"
 
-void	draw_square(void *mlx, void *win, int x, int y, int side, int color)
-{
-	int i;
-	int	j;
-
-	i = 0;
-	while (i < side)
-	{
-		j = 0;
-		while (j < side)
-		{
-			mlx_pixel_put(mlx, win, x+j, y+i, color);
-			j++;
-		}
-		i++;
-	}
-}
-
 void	draw_item(void *mlx, void *win, char c, int x, int y, t_image image)
 {
 	if (c == '1')
@@ -42,7 +24,10 @@ void	draw_item(void *mlx, void *win, char c, int x, int y, t_image image)
 	else if (c == '0')
 		mlx_put_image_to_window(mlx, win, image.floor, x, y);
 	else if (c == 'C')
+	{
+		mlx_put_image_to_window(mlx, win, image.floor, x, y);
 		mlx_put_image_to_window(mlx, win, image.collectible, x, y);
+	}
 	else if (c == 'E')
 		mlx_put_image_to_window(mlx, win, image.exit, x, y);
 }
@@ -54,8 +39,8 @@ void	load_image(void *mlx, t_image *image)
 	raf = 0;
 	image->wall = mlx_xpm_file_to_image(mlx, "xpm/wall.xpm", &raf, &raf);
 	image->floor = mlx_xpm_file_to_image(mlx, "xpm/floor.xpm", &raf, &raf);
-	image->exit = mlx_xpm_file_to_image(mlx, "xpm/exit.xpm", &raf, &raf);
-	image->collectible = mlx_xpm_file_to_image(mlx, "xpm/collectible.xpm", &raf, &raf);
+	image->exit = mlx_xpm_file_to_image(mlx, "xpm/exit1.xpm", &raf, &raf);
+	image->collectible = mlx_xpm_file_to_image(mlx, "xpm/burger.xpm", &raf, &raf);
 	image->cartman = mlx_xpm_file_to_image(mlx, "xpm/cartman.xpm", &raf, &raf);
 }
 void	draw_map(void *mlx, void *win, char **map, t_image image)
